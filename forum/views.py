902fc128265ff4ca09e25ba5197c934c045c14e3
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-28 20:44:35 +0200

    add questions_list in views

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Question , Answer
 # Create your views here.
+
+def questions_list(request):
+    data = Question.objects.all()
+    return render(request, 'forum/questions_list', {'data':data})
+
+
