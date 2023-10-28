a07684f31f095665345c5fdd1573e55922c745cc
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-28 20:24:37 +0200

    create forum app and add taggit

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
+from taggit.managers import TaggableManager
 # Create your models here.
+
+class Question (models.Model):
+    
+    question = models.CharField(max_length=200)
+    content = models.TextField(max_length=30000)
+    created_at = models.DateTimeField(default=timezone.now)
+    tags = TaggableManager()
