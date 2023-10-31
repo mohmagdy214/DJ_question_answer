e21c2970fff078725b3bfc39811c87bf55d4784b
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-31 03:00:45 +0200

    add search in questions and answers

--- forum/views.py
+++ forum/views.py
@@ -4,14 +4,25 @@ from .forms import AnswerForm
 # Create your views here.
 
 def question_list(request):
-    data = Question.objects.all()
-    return render(request, 'forum/question_list', {'data':data})
+    search = Question.objects.all()
+    questions = None
+    if 'search_name' in request.GET:
+        questions = request.GET['search_name']
+        if questions:
+            search = search.filter(question__icontains=questions)
+
+    return render(request, 'forum/question_list', {'questions':search})
 
 
 
 def question_detail(request,question_id):
     question = Question.objects.get(id=question_id)
-    answers = Answer.objects.filter(question=question)
+    search2 = Answer.objects.filter(question=question)
+    answers = None
+    if 'search_name2' in request.GET:
+        answers = request.GET['search_name2']
+        if answers:
+            search2 = search2.filter(answer__icontains=answers)
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
@@ -23,7 +34,7 @@ def question_detail(request,question_id):
         form = AnswerForm()
 
 
-    return render(request, 'forum/question_detail', {'question':question , 'answers':answers,'form':form})
+    return render(request, 'forum/question_detail', {'question':question , 'answers':search2,'form':form})
 
 
 
